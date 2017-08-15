36d95ba473c25fe674deb2c2a9301ef8e878a321
Jose Toledo <jotatoledon@gmail.com>
2017-08-15 14:13:14 +0200

    chore(main module): added dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import 'rxjs/add/operator/catch';
 
+import { CredentialInterceptorProvider, ErrorInterceptorProvider } from './interceptors';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +11,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    CredentialInterceptorProvider,
+    ErrorInterceptorProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
