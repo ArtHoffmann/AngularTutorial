3d59ee7195be10da050d6699ac90680fee4b056a
Jose Toledo <jotatoledon@gmail.com>
2017-08-16 08:43:59 +0200

    chore(model): minor changes
    * Applied camelCase to properties

--- src/app/models/person/person.ts
+++ src/app/models/person/person.ts
@@ -1,9 +1,11 @@
 export class Person {
     constructor(
-        public FirstName?: string,
-        public LastName?: string,
-        public DateOfBirth?: Date
-    ) { }
+        public firstName?: string,
+        public lastName?: string,
+        public dateOfBirth?: Date
+    ) {
+        this.dateOfBirth = new Date();
+    }
 }
 
 export interface PersonPresentation {
