7a02c978179e6d752cf29b1f4cde9b72750db429
Jose Toledo <fixed-term.JoseIgnacio.ToledoNavarro@de.bosch.com>
2017-08-16 14:42:45 +0200

    feature(models/person): added person details model

--- src/app/models/person/person.ts
+++ src/app/models/person/person.ts
@@ -14,3 +14,7 @@ export interface PersonPresentation {
     lastName: string;
     dateOfBirth: string;
 }
+
+export interface PersonDetails extends PersonPresentation {
+    totalAddresses: number;
+}
