b8f27e81c9a55d87f3bc07f21e31de2f2043d1d4
Jose Toledo <fixed-term.JoseIgnacio.ToledoNavarro@de.bosch.com>
2017-08-16 15:54:36 +0200

    feature(address): added list page component

---
+++ src/app/address/containers/addresses-list-page/addresses-list-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addresses-list-page',
+  templateUrl: './addresses-list-page.component.html',
+  styleUrls: ['./addresses-list-page.component.css']
+})
+export class AddressesListPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
