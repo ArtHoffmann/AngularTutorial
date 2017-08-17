3a87497f18a4a8cec65b09c62a8ad9cf8adb0b78
Jose Toledo <jotatoledon@gmail.com>
2017-08-17 11:04:50 +0200

    chore(person/table): reworked internals

--- src/app/person/components/persons-table/persons-table.component.ts
+++ src/app/person/components/persons-table/persons-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
+import { ITdDataTableColumn } from '@covalent/core';
 
 import { PersonPresentation } from './../../../models/person/person';
 
@@ -17,6 +18,24 @@ export class PersonsTableComponent {
 
   @Output()
   onSelect = new EventEmitter<PersonPresentation>();
+
+  columns: ITdDataTableColumn[] = [
+    {
+      name: 'actions', label: 'Actions'
+    },
+    {
+      name: 'id', label: 'Id'
+    },
+    {
+      name: 'firstName', label: 'First Name'
+    },
+    {
+      name: 'lastName', label: 'Last Name'
+    },
+    {
+      name: 'dateOfBirth', label: 'Date of Birth'
+    }
+  ];
 
   triggerDelete(person: PersonPresentation) {
     this.onDelete.emit(person);
